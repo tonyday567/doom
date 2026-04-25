6cbf31b1f1d79c28a6888fbbe0733ca6a60e6e84
Tony Day <tonyday567@gmail.com>
2026-04-25 13:57:15 +1000

    add symbol shortcuts on SPC d (circles, marks, symbols)

--- config.el
+++ config.el
@@ -95,6 +95,35 @@
       "M-SPC" #'cycle-spacing
       [remap ibuffer] #'ibuffer-jump)
 
+;; mg symbol shortcuts on SPC d
+(map! :leader
+      :prefix ("d" . "mark/symbol")
+      ;; circles
+      (:prefix ("c" . "circle/color")
+       :desc "Red"    "r" (lambda () (interactive) (insert "🔴"))
+       :desc "Green"  "g" (lambda () (interactive) (insert "🟢"))
+       :desc "Orange" "o" (lambda () (interactive) (insert "🟠"))
+       :desc "Purple" "p" (lambda () (interactive) (insert "🟣"))
+       :desc "Blue"   "b" (lambda () (interactive) (insert "🔵")))
+      ;; marks
+      (:prefix ("m" . "mark")
+       :desc "Elab (⟜)"       "e" (lambda () (interactive) (insert "⟜"))
+       :desc "Action (⟝)"     "a" (lambda () (interactive) (insert "⟝"))
+       :desc "Weave (⧈)"      "w" (lambda () (interactive) (insert "⧈"))
+       :desc "Hexagon (⬡)"    "h" (lambda () (interactive) (insert "⬡"))
+       :desc "Lozenge (◊)"    "l" (lambda () (interactive) (insert "◊"))
+       :desc "Right gate (⟞)" "]" (lambda () (interactive) (insert "⟞")))
+      ;; symbols from circuits
+      (:prefix ("s" . "symbol")
+       :desc "Lift (η)"    "l" (lambda () (interactive) (insert "η"))
+       :desc "Lower (ε)"   "w" (lambda () (interactive) (insert "ε"))
+       :desc "Compose (⊙)" "." (lambda () (interactive) (insert "⊙"))
+       :desc "Push (⊲)"    "p" (lambda () (interactive) (insert "⊲"))
+       :desc "Run (⥁)"     "r" (lambda () (interactive) (insert "⥁"))
+       :desc "Loop (↬)"    "o" (lambda () (interactive) (insert "↬"))
+       :desc "Trace (⥀)"   "t" (lambda () (interactive) (insert "⥀"))
+       :desc "Untrace (↯)" "u" (lambda () (interactive) (insert "↯"))))
+
 (setq doom-modeline-lsp-icon nil)
 (setq doom-modeline-buffer-encoding nil)
 (setq doom-modeline-buffer-state-icon nil)
